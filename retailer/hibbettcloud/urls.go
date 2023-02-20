81c4832e60d020743f2d0de4b11fd71a7bbf75b8
stefan araujo <saraujo@ufl.edu>
2023-02-20 16:36:02 -0600

    added commonality

--- retailer/hibbettcloud/urls.go
+++ retailer/hibbettcloud/urls.go
@@ -2,7 +2,6 @@ package hibbettcloud
 
 const serverUrl string = `https://hibbett-mobileapi.prolific.io/users/`
 
-
 func getPaymentIdUrlString(customerID string) string {
 	return serverUrl + customerID + "/payment_methods"
 }
