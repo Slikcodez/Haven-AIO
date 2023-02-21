0ab268cf4ae577899e81c0f332e1c664f29b9005
cyrusnaficy <cyrus.naficy@gmail.com>
2023-02-21 15:03:02 -0500

    merge constants

--- constants/const.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const ServerUrl string = `http://38.102.8.15`
-
-const HibbettURL string = `https://hibbett-mobileapi.prolific.io/users/`
-
-func GetPaymentIdUrlString(customerID string) string {
-	return HibbettURL + customerID + "/payment_methods"
-}
--- constants/constants.go
+++ constants/constants.go
@@ -3,9 +3,14 @@ package constants
 import "fmt"
 
 const ServersUrl string = `http://38.102.8.15`
+const HibbettURL string = `https://hibbett-mobileapi.prolific.io/users/`
 
 const BearerToken string = `pk_cqzp7m2w4zsl5jlx233swz3uj7prgkmc5lmzg205`
 
 func LogStatus(thread string, message string) {
 	fmt.Println("Thread " + thread + ": " + message)
+}
+
+func GetPaymentIdUrlString(customerID string) string {
+	return HibbettURL + customerID + "/payment_methods"
 }
