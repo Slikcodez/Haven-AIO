27cf145a41503eb04afc2aa6ae455e8fff694e3c
Anthony <anthonymariod24@gmail.com>
2023-03-11 10:19:13 -0600

    changed authorization px on place order

--- retailer/hibbettcloud/placeorder.go
+++ retailer/hibbettcloud/placeorder.go
@@ -97,26 +97,30 @@ type Order struct {
 
 func (user *HibbettBase) placeOrderRequest() (res []byte, err error) {
 
-	pxBase := "2"
+	pxBase := "1"
 
 	res, err = client.TlsRequest(client.TLSParams{
 		Client: user.client,
 		Method: http.MethodPost,
 		Url:    fmt.Sprintf(`https://hibbett-mobileapi.prolific.io/ecommerce/cart/%s/place_order?cardSecurityCode=%s&customer=%s&phone=&oneTapCheckout=true&firstName=&optIn=false`, user.cartId, user.cvv, user.customerId),
 		Headers: http.Header{
-			"Accept":              {"*/*"},
-			"Accept-Encoding":     {"br;q=1.0, gzip;q=0.9, deflate;q=0.8"},
-			"Accept-Language":     {"en-US;q=1.0"},
-			"Connection":          {"keep-alive"},
-			"Content-Type":        {"application/json; charset=utf-8"},
-			"platform":            {"ios"},
-			"version":             {"6.3.0"},
-			"Authorization":       {"Bearer " + user.sessionId},
-			"x-api-key":           {"0PutYAUfHz8ozEeqTFlF014LMJji6Rsc8bpRBGB0"},
-			"X-PX-AUTHORIZATION":  {pxBase}, //1 also works
-			"X-PX-ORIGINAL-TOKEN": {pxBase + ":" + constants.RandString()},
-			"Cache-Control":       {constants.RandString() + ", no-cache, no-store, must-revalidate"},
-			"User-Agent":          {user.userAgent},
+			"Accept":                        {"*/*"},
+			"Accept-Encoding":               {"br;q=1.0, gzip;q=0.9, deflate;q=0.8"},
+			"Accept-Language":               {"en-US;q=0.9"},
+			"Connection":                    {"close"},
+			"Content-Type":                  {"application/json; charset=utf-8"},
+			"platform":                      {"ios"},
+			"version":                       {"6.3.0"},
+			"Authorization":                 {"Bearer " + user.sessionId},
+			"x-api-key":                     {"0PutYAUfHz8ozEeqTFlF014LMJji6Rsc8bpRBGB0"},
+			"X-PX-AUTHORIZATION":            {pxBase}, //1 also works
+			"X-PX-ORIGINAL-TOKEN":           {pxBase + ":" + constants.RandString()},
+			"Cache-Control":                 {constants.RandString() + ", no-cache, no-store, must-revalidate"},
+			"Pragma":                        {"no-cache"},
+			"Access-Control-Request-Method": {"POST"},
+			"Priority":                      {"1 1 0"},
+			"TE":                            {"Trailers"},
+			"User-Agent":                    {user.userAgent},
 		},
 		Body:             nil,
 		ExpectedResponse: 200,
