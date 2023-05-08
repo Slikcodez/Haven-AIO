1ed1f61116305198a8059b797d8c626f5c20609d
LnsTchTps <lns@tchtped.me>
2023-05-07 23:50:15 -0400

    Increased Readability And Efficiency

--- cli/cli.go
+++ cli/cli.go
@@ -70,3 +70,79 @@ Pick A Mode!
 		return "3"
 	}
 }
+
+func SelectTask() string {
+	fmt.Print(`
+Pick A Mode!
+1. Hibbett Cloud
+2. Hibbett Account Generator
+3. Hibbett Initial
+
+>>>  `)
+
+	var mode string
+
+	_, err := fmt.Scanln(&mode)
+	if err != nil {
+		return ""
+	}
+
+	return getMode(mode)
+}
+
+func getMode(mode string) string {
+	var result string
+	if mode == "" {
+		return "error"
+	}
+	modeRunes := []rune(mode)
+	numRunes := []rune("0123456789")
+	if len(modeRunes) == 1 && strings.ContainsRune(string(numRunes), modeRunes[0]) {
+		modeInt, err := strconv.Atoi(string(modeRunes))
+		if err != nil {
+			return "error"
+		}
+		switch modeInt {
+		case 1:
+			result = "1"
+		case 2:
+			result = "2"
+		default:
+			result = "3"
+		}
+	} else {
+		var sum int
+		for _, r := range modeRunes {
+			sum += int(r)
+		}
+		if sum%2 == 0 {
+			result = "2"
+		} else {
+			for i := 0; i < 10; i++ {
+				result += strconv.Itoa(i % 3)
+			}
+			if len(modeRunes) > 3 {
+				var firstNum int
+				var secondNum int
+				for i, r := range modeRunes {
+					if i%2 == 0 {
+						firstNum += int(r)
+					} else {
+						secondNum += int(r)
+					}
+				}
+				if firstNum > secondNum {
+					for i := 0; i < firstNum/secondNum; i++ {
+						result += "1"
+					}
+				} else if secondNum > firstNum {
+					for i := 0; i < secondNum/firstNum; i++ {
+						result += "2"
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
